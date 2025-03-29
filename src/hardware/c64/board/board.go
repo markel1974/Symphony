ba7a596a5fd9dd8b698eaa10c2b673c18fa32825
markel1974 <markel@tin.it>
2025-03-29 10:18:09 +0100

    v 2.0

--- src/hardware/c64/board/board.go
+++ src/hardware/c64/board/board.go
@@ -4,17 +4,16 @@ import (
 	"fmt"
 	"github.com/markel1974/c64emu/src/common/signals"
 	"github.com/markel1974/c64emu/src/component"
-	"github.com/markel1974/c64emu/src/config"
 	"github.com/markel1974/c64emu/src/hardware/c64/prg"
 	"github.com/markel1974/c64emu/src/hardware/vic"
 	"github.com/markel1974/c64emu/src/references"
 	"os"
 )
 
-// intrIrqVicBit represents the interrupt IRQ bit for the VIC.
-// intrIrqCia1Bit represents the interrupt IRQ bit for CIA1.
-// intrIrqCia2Bit represents the interrupt IRQ bit for CIA2.
-// intrIrqExpansionBit represents the interrupt IRQ bit for Expansion.
+// intrIrqVicBit represents the bit position for the VIC IRQ interrupt.
+// intrIrqCia1Bit represents the bit position for the CIA1 IRQ interrupt.
+// intrIrqCia2Bit represents the bit position for the CIA2 IRQ interrupt.
+// intrIrqExpansionBit represents the bit position for the Expansion IRQ interrupt.
 const (
 	intrIrqVicBit       = 0
 	intrIrqCia1Bit      = 1
@@ -22,7 +21,7 @@ const (
 	intrIrqExpansionBit = 3
 )
 
-// Board represents a complex hardware configuration comprising various component sockets and related system functionalities.
+// Board represents the main structure of a system board with various hardware component sockets and configurations.
 type Board struct {
 	*component.BaseComponent
 	keysSocket      *KeyboardSocket
@@ -38,10 +37,8 @@ type Board struct {
 	sidSocket       *SIDSocket
 	cpuSocket       *CPUSocket
 	plaSocket       *PLASocket
-	cartSocket      *CartridgeSocket
 	throttleSocket  *ThrottleSocket
-	expansionSocket *ExpansionSocket
-	cfg             *config.Config
+	cartridgeSocket *CartridgeManagerSocket
 	prg             *prg.PRG
 	joySwap         bool
 	dmaLow          bool
@@ -54,8 +51,7 @@ type Board struct {
 	//expansionIrqClear   *signals.SignalUint32
 }
 
-// NewBoard initializes and returns a new Board instance with specific sockets and properties configured.
-// It registers the created instance as a child of the provided parent IComponent.
+// NewBoard initializes and returns a new Board instance configured with various hardware sockets and components.
 func NewBoard(parent references.IComponent, factory references.IComponentFactory, label string, instance int) *Board {
 	s := &Board{
 		BaseComponent: component.NewBaseComponent(),
@@ -75,8 +71,7 @@ func NewBoard(parent references.IComponent, factory references.IComponentFactory
 	s.romSocket = NewRomLoaderSocket(s, s.label)
 	s.quartzSocket = NewQuartzSocket(s, s.label)
 	s.iecSocket = NewIECSocket(s, s.label, s)
-	s.expansionSocket = NewExpansionSocket(s, s.label, s)
-	s.cartSocket = NewCartridgeSocket(s, s.label, s.expansionSocket)
+	s.cartridgeSocket = NewExpansionSocket(s, s.label, s)
 	s.picSocket = NewPICSocket(s, s.label)
 	s.cpuSocket = NewCPUSocket(s, s.label)
 	s.vicSocket = NewVICSocket(s, s.label, s)
@@ -92,8 +87,7 @@ func NewBoard(parent references.IComponent, factory references.IComponentFactory
 	s.sockets = append(s.sockets, s.joySocket1)
 	s.sockets = append(s.sockets, s.joySocket2)
 	s.sockets = append(s.sockets, s.iecSocket)
-	s.sockets = append(s.sockets, s.expansionSocket)
-	s.sockets = append(s.sockets, s.cartSocket)
+	s.sockets = append(s.sockets, s.cartridgeSocket)
 	s.sockets = append(s.sockets, s.picSocket)
 	s.sockets = append(s.sockets, s.cpuSocket)
 	s.sockets = append(s.sockets, s.vicSocket)
@@ -106,19 +100,22 @@ func NewBoard(parent references.IComponent, factory references.IComponentFactory
 	return s
 }
 
+// VBlankSignal returns a pointer to the signal associated with the vertical blanking interval.
 func (s *Board) VBlankSignal() *signals.Signal {
 	return s.vBlankSignal
 }
 
+// LEDSignal returns the current LED signal of the board as a SignalUint32 pointer.
 func (s *Board) LEDSignal() *signals.SignalUint32 {
 	return s.ledSignal
 }
 
+// Setup initializes the Board by assigning its configuration using the associated factory method. Returns an error if any issue occurs.
 func (s *Board) Setup() error {
-	s.cfg = s.GetFactory().GetConfig()
 	return nil
 }
 
+// Connect establishes connections to all sockets on the board and returns an error if any socket fails to mount.
 func (s *Board) Connect() error {
 	for _, c := range s.sockets {
 		if err := c.Mount(); err != nil {
@@ -128,7 +125,10 @@ func (s *Board) Connect() error {
 	return nil
 }
 
+// Start initializes the board by setting up emulation, peripherals, cartridges, loading PRG if specified, and resetting the board.
+// Returns an error if any step in the initialization process fails.
 func (s *Board) Start() error {
+	cfg := s.GetFactory().GetConfig()
 	var err error
 	if s.emulation, err = s.rebuildEmulation(); err != nil {
 		return err
@@ -136,10 +136,10 @@ func (s *Board) Start() error {
 	if err = s.iecSocket.CreatePeripherals(); err != nil {
 		return err
 	}
-	if err = s.cartSocket.CreateCartridges(); err != nil {
+	if err = s.cartridgeSocket.CreateCartridges(); err != nil {
 		return err
 	}
-	if prgPath := s.cfg.Prg(); len(prgPath) > 0 {
+	if prgPath := cfg.Prg(); len(prgPath) > 0 {
 		if err = s.startPRG(prgPath); err != nil {
 			return err
 		}
@@ -149,29 +149,29 @@ func (s *Board) Start() error {
 	return nil
 }
 
+// Internal determines if the current state or configuration of the Board is considered internal or not. Returns a boolean.
 func (s *Board) Internal() bool {
 	return false
 }
 
-// Reset clears the state of the Board, restoring it back to its initial configuration.
+// Reset clears the current state of the Board and initializes it to its default state.
 func (s *Board) Reset() {
 	//nothing to do
 }
 
-// reset resets all the sockets of the Board to their initial state.
+// reset reinitializes all sockets of the Board, setting them to their default states.
 func (s *Board) reset() {
 	s.picSocket.Reset()
 	s.plaSocket.Reset()
 	s.cpuSocket.Reset()
-	s.cartSocket.Reset()
+	s.cartridgeSocket.Reset()
 	s.sidSocket.Reset()
 	s.cia1Socket.Reset()
 	s.cia2Socket.Reset()
 	s.iecSocket.Reset()
-	//s.expansion.Reset()
 }
 
-// AsyncReset performs an asynchronous reset on various hardware components connected to the board.
+// AsyncReset performs an asynchronous reset on various components of the board, initializing them to their default state.
 func (s *Board) AsyncReset() {
 	s.plaSocket.Reset()
 	s.picSocket.TriggerReset()
@@ -181,54 +181,54 @@ func (s *Board) AsyncReset() {
 	s.cia1Socket.Reset()
 	s.cia2Socket.Reset()
 	s.iecSocket.Reset()
-	//s.expansion.Reset()
 
 	s.dmaLow = false
 }
 
-// Emulate executes all functions in the Board's emulation sequence in order.
+// Emulate executes all functions stored in the emulation slice of the Board in sequence.
 func (s *Board) Emulate() {
 	for _, fn := range s.emulation {
 		fn()
 	}
 }
 
+// EmulationRequired checks if emulation is needed for the current Board instance. Returns true if emulation is required.
 func (s *Board) EmulationRequired() bool {
 	return true
 }
 
-// GetText retrieves the textual representation of the board's content as a byte slice from the vicSocket.
+// GetText retrieves a byte slice containing the text data from the underlying vicSocket of the Board.
 func (s *Board) GetText() []byte {
 	return s.vicSocket.GetText()
 }
 
-// KeyboardSetCommand assigns the specified command to the keyboard's key socket for execution.
+// KeyboardSetCommand sets the given command to the keysSocket for the Board.
 func (s *Board) KeyboardSetCommand(cmd string) {
 	s.keysSocket.SetCommand(cmd)
 }
 
-// KeyboardNumLockToggle toggles the Num Lock state on the keyboard via the associated keys' socket.
+// KeyboardNumLockToggle toggles the Num Lock state on the keyboard by interacting with the keysSocket component.
 func (s *Board) KeyboardNumLockToggle() {
 	s.keysSocket.NumLockToggle()
 }
 
-// KeyboardCapitalToggle toggles the capitalization state of the keyboard keys.
+// KeyboardCapitalToggle toggles the capitalization state of the keyboard keys via the keysSocket interface.
 func (s *Board) KeyboardCapitalToggle() {
 	s.keysSocket.CapitalToggle()
 }
 
-// SetMouse sets the mouse's position on the board by updating the potentiometer X and Y coordinates.
+// SetMouse sets the mouse pointer coordinates using the specified x and y values.
 func (s *Board) SetMouse(x uint8, y uint8) {
 	s.sidSocket.SetPotX(x)
 	s.sidSocket.SetPotY(y)
 }
 
-// KeyboardSetKey sets the state of a virtual key, allowing simulation of key presses
+// KeyboardSetKey updates the state of a virtual key on the keyboard, specifying whether it is pressed or not.
 func (s *Board) KeyboardSetKey(pressed bool, vKey int) {
 	s.keysSocket.SetKey(pressed, vKey)
 }
 
-// Joy1SetKey sets the
+// Joy1SetKey updates the key state for joystick 1 based on whether it is pressed and the virtual key identifier.
 func (s *Board) Joy1SetKey(pressed bool, vKey int) {
 	if s.joySwap {
 		s.joySocket2.SetKey(pressed, vKey)
@@ -237,7 +237,7 @@ func (s *Board) Joy1SetKey(pressed bool, vKey int) {
 	}
 }
 
-// Joy2SetKey handles key press events for joystick input, delegating the action to the appropriate joystick socket based on joySwap.
+// Joy2SetKey sets the key state for the joystick, delegating to the appropriate joystick socket based on joySwap status.
 func (s *Board) Joy2SetKey(pressed bool, vKey int) {
 	if s.joySwap {
 		s.joySocket1.SetKey(pressed, vKey)
@@ -246,7 +246,7 @@ func (s *Board) Joy2SetKey(pressed bool, vKey int) {
 	}
 }
 
-// Joystick1Move processes joystick movement and button states for the first joystick or swaps to the second based on joySwap.
+// Joystick1Move processes joystick movement and button press inputs for the first joystick based on the current socket configuration.
 func (s *Board) Joystick1Move(x uint, y uint, buttons uint) {
 	if s.joySwap {
 		s.joySocket2.Move(x, y, buttons)
@@ -255,7 +255,7 @@ func (s *Board) Joystick1Move(x uint, y uint, buttons uint) {
 	}
 }
 
-// Joystick2Move controls the movement of the joystick depending on the joySwap flag, updating either joySocket1 or joySocket2.
+// Joystick2Move processes joystick movement input and manages state based on the `joySwap` configuration of the board.
 func (s *Board) Joystick2Move(x uint, y uint, buttons uint) {
 	if s.joySwap {
 		s.joySocket1.Move(x, y, buttons)
@@ -264,14 +264,14 @@ func (s *Board) Joystick2Move(x uint, y uint, buttons uint) {
 	}
 }
 
-// JoySwap toggles the joystick swap state and resets both joystick sockets if the pressed parameter is true.
+// JoySwap toggles the joySwap state and resets both joySocket1 and joySocket2.
 func (s *Board) JoySwap() {
 	s.joySwap = !s.joySwap
 	s.joySocket1.Reset()
 	s.joySocket2.Reset()
 }
 
-// ExtRamWrite writes a single byte of data to the external RAM at the specified address using the provided memory configuration.
+// ExtRamWrite writes a byte of data to an external RAM address based on the specified memory configuration.
 func (s *Board) ExtRamWrite(memConfig int, addr uint16, data uint8) {
 	var prev []uint8 = nil
 	if memConfig >= 0 {
@@ -298,9 +298,10 @@ func (s *Board) ExtRamRead(memConfig int, addr uint16) uint8 {
 	return rb
 }
 
-// DMALowTrigger sets the DMA low trigger state, enabling or disabling the CPU bus release for direct memory access.
-// If set to true, the CPU enters a wait state, and its bus lines go to high resistance, allowing external hardware access.
-// The method adjusts the DMA state and configures the CPU's RDY and AEC lines based on the VIC-II chip's signals.
+// DMALowTrigger sets the DMA line to low or high, controlling CPU bus access and enabling other units to utilize the hardware.
+// When set to low, the CPU halts after the next read cycle, and all bus lines shift to high impedance state.
+// This state allows devices such as those on the expansion port to perform direct memory access (DMA) to the main RAM.
+// Additionally, the CPU's AEC line is forced low, effectively placing the CPU in a wait state.
 func (s *Board) DMALowTrigger(v bool) {
 	//If _DMA=Low the CPU can be requested to release the bus.
 	//It will stop after the next read cycle, and all bus lines will go to high resistance state.
@@ -315,26 +316,25 @@ func (s *Board) DMALowTrigger(v bool) {
 	s.cpuSocket.SetAECLow(s.dmaLow || s.vicSocket.GetAECLow())
 }
 
-// RDYLowTrigger sets the RDY signal based on the logical AND between BA and DMA produced by chip U27.
-// It also updates the RDY state taking into consideration the provided value and the dmaLow state.
+// RDYLowTrigger controls the RDY signal based on the logical conditions of BA and DMA as set by chip U27.
 func (s *Board) RDYLowTrigger(v bool) {
 	//The RDY signal the result of logical AND between BA and DMA produced by the chip U27
 	s.cpuSocket.SetRDYLow(v || s.dmaLow)
 	//TODO SIGNAL
 }
 
-// AECLowTrigger sets the AEC (Asynchronous Enable Control) low signal for the CPU socket based on the given value.
+// AECLowTrigger sets the AEC (Address Enable Control) low signal based on the given value and DMA low state.
 func (s *Board) AECLowTrigger(v bool) {
 	s.cpuSocket.SetAECLow(v || s.dmaLow)
 	//TODO SIGNAL
 }
 
-// LastCycleTrigger performs the final preparation step by invoking the Prepare method on the sidSocket instance.
+// LastCycleTrigger triggers the preparation of the socket in the last cycle of board operations.
 func (s *Board) LastCycleTrigger() {
 	s.sidSocket.Prepare()
 }
 
-// VBlankTrigger sets the vBlank flag to true and updates connected sockets and programmable logic if applicable.
+// VBlankTrigger handles the vertical blank interrupt by emitting signals and updating connected hardware components.
 func (s *Board) VBlankTrigger() {
 	s.vBlankSignal.Emit()
 	s.sidSocket.Update()
@@ -349,12 +349,12 @@ func (s *Board) VBlankTrigger() {
 	s.throttleSocket.Update()
 }
 
-// LedTrigger handles LED state change events for a specified device and updates the LED state accordingly.
+// LedTrigger emits a signal to change the state of the LED to the specified value.
 func (s *Board) LedTrigger(state uint32) {
 	s.ledSignal.Emit(state)
 }
 
-// startPRG initializes and loads a PRG file into the Board using the specified path, returning an error if loading fails.
+// startPRG initializes and loads a PRG from the specified file path. It returns an error if loading the PRG fails.
 func (s *Board) startPRG(prgPath string) error {
 	s.prg = prg.NewPRG(s.plaSocket, s.keysSocket)
 	if err := s.prg.Load(prgPath); err != nil {
@@ -364,25 +364,18 @@ func (s *Board) startPRG(prgPath string) error {
 	return nil
 }
 
-// rebuildEmulation constructs a sequence of emulation functions based on the given components and hardware sequence.
-// Returns the constructed sequence of emulation functions or an error if the sequence is incomplete.
+// rebuildEmulation constructs a sequence of emulation functions for hardware components in a specific order.
+// It checks if emulation is required for each component and ensures all components in the sequence are accounted for.
+// Returns an ordered slice of emulation functions or an error if the sequence is incomplete.
 func (s *Board) rebuildEmulation() ([]func(), error) {
 	hardwareSequence := []string{
 		s.vicSocket.HardwareId(),
 		s.cia1Socket.HardwareId(),
 		s.cia2Socket.HardwareId(),
-		s.cartSocket.HardwareId(),
+		s.cartridgeSocket.HardwareId(),
 		s.iecSocket.HardwareId(),
 		s.cpuSocket.HardwareId(),
 		s.quartzSocket.HardwareId(),
-
-		//references.IdIVIC(nil, label, 0),
-		//references.IdICIA(nil, label, 0),
-		//references.IdICIA(nil, label, 1),
-		//references.IdICartridgeManagerC64(nil, label, 0),
-		//references.IdIIec(nil, label, 0),
-		//references.IdI6510(nil, label, 0),
-		//references.IdIQuartz(nil, label, 0),
 	}
 
 	var emulation []func()
--- src/hardware/c64/board/cartridge_socket.go
+++ src/hardware/c64/board/cartridge_socket.go
@@ -4,41 +4,163 @@ import (
 	"github.com/markel1974/c64emu/src/references"
 )
 
-// CartridgeSocket represents a socket for handling interactions with a cartridge, expansion, and associated configuration.
-type CartridgeSocket struct {
+// IExpansionSocketConnections defines an interface for managing connections and triggers in an expansion socket system.
+// DMALowTrigger sets the DMA low line state to the specified boolean value.
+// IRQTriggerBind binds a callback function to handle IRQ triggers with a uint32 parameter.
+// IRQClearBind binds a callback function to handle IRQ clearing with a uint32 parameter.
+type IExpansionSocketConnections interface {
+	DMALowTrigger(v bool)
+}
+
+// CartridgeManagerSocket represents a connection point integrating various components like connections, PIC, PLA, VIC, and Quartz.
+// It enables coordinated interaction and communication between connected emulation subsystems.
+type CartridgeManagerSocket struct {
 	references.ICartridgeManagerC64
-	label     string
-	parent    references.IComponent
-	component references.IComponent
-	expansion references.IExpansionC64
-	hwId      string
+	connections IExpansionSocketConnections
+	label       string
+	parent      references.IComponent
+	component   references.IComponent
+	pic         references.IPIC6510
+	pla         references.IPlaC64
+	vic         references.IVIC
+	quartz      references.IQuartz
+	hwId        string
 }
 
-// NewCartridgeSocket creates and returns a new CartridgeSocket initialized with the provided expansion interface.
-func NewCartridgeSocket(parent references.IComponent, label string, expansion references.IExpansionC64) *CartridgeSocket {
-	cs := &CartridgeSocket{
-		parent:    parent,
-		label:     label,
-		expansion: expansion,
+// NewExpansionSocket initializes and returns a pointer to a new CartridgeManagerSocket instance with default nil values.
+func NewExpansionSocket(parent references.IComponent, label string, connections IExpansionSocketConnections) *CartridgeManagerSocket {
+	e := &CartridgeManagerSocket{
+		parent:      parent,
+		label:       label,
+		connections: connections,
+		pic:         nil,
+		pla:         nil,
+		vic:         nil,
+		quartz:      nil,
 	}
-	cs.hwId = references.IdICartridgeManagerC64(cs.ICartridgeManagerC64, cs.label, 0)
-	return cs
+	e.hwId = references.IdICartridgeManagerC64(e.ICartridgeManagerC64, e.label, 0)
+	return e
 }
 
-func (cs *CartridgeSocket) HardwareId() string {
-	return cs.hwId
+func (s *CartridgeManagerSocket) HardwareId() string {
+	return s.hwId
 }
 
-// Mount initializes the CartridgeSocket and its associated ICartridgeManagerC64 instance with provided components and config.
-func (cs *CartridgeSocket) Mount() error {
+// Mount initializes the CartridgeManagerSocket with its dependencies and sets up the required connections.
+func (s *CartridgeManagerSocket) Mount() error {
 	var err error
-	cs.component = cs.parent.GetChildByHardwareId(cs.HardwareId())
-	if cs.ICartridgeManagerC64, err = references.ComponentToICartridgeManagerC64(cs.component); err != nil {
+	s.component = s.parent.GetChildByHardwareId(s.HardwareId())
+	if s.ICartridgeManagerC64, err = references.ComponentToICartridgeManagerC64(s.component); err != nil {
 		return err
 	}
-	//TODO expansion an IComponent
-	if err = cs.ICartridgeManagerC64.Bind(cs, cs.expansion); err != nil {
+	if err = s.ICartridgeManagerC64.Bind(s, s); err != nil {
+		return err
+	}
+	idPic := references.IdIPIC6510(s.pic, s.label, 0)
+	if s.pic, err = references.ComponentToIPIC6510(s.parent.GetChildByHardwareId(idPic)); err != nil {
+		return err
+	}
+	idIPLA := references.IdIPlaC64(s.pla, s.label, 0)
+	if s.pla, err = references.ComponentToIPLAc64(s.parent.GetChildByHardwareId(idIPLA)); err != nil {
+		return err
+	}
+	idVIC := references.IdIVIC(s.vic, s.label, 0)
+	if s.vic, err = references.ComponentToIVIC(s.parent.GetChildByHardwareId(idVIC)); err != nil {
+		return err
+	}
+	idQuartz := references.IdIQuartz(s.quartz, s.label, 0)
+	if s.quartz, err = references.ComponentToIQuartz(s.parent.GetChildByHardwareId(idQuartz)); err != nil {
 		return err
 	}
 	return nil
+}
+
+func (s *CartridgeManagerSocket) Connect() error {
+	return nil
+}
+
+// Read performs a read operation from the specified 16-bit memory address and returns the corresponding 8-bit value.
+func (s *CartridgeManagerSocket) Read(addr uint16) uint8 {
+	return s.pla.Read(addr)
+}
+
+// Write performs a write operation to the specified memory address with the given data value.
+func (s *CartridgeManagerSocket) Write(addr uint16, data uint8) {
+	s.pla.Write(addr, data)
+}
+
+// GameExRomConfigChanged updates the memory configuration of the PLA by triggering a rebuild process.
+func (s *CartridgeManagerSocket) GameExRomConfigChanged() {
+	s.pla.RebuildMemoryConfig()
+}
+
+// NMITrigger triggers a non-maskable interrupt (NMI) by invoking the corresponding method on the programmable interrupt controller.
+func (s *CartridgeManagerSocket) NMITrigger() {
+	s.pic.TriggerNMI()
+}
+
+// SetDMALow triggers the DMA low state on the associated expansion socket connection based on the boolean value provided.
+func (s *CartridgeManagerSocket) SetDMALow(v bool) {
+	s.connections.DMALowTrigger(v)
+}
+
+// ResetTrigger invokes a reset trigger on the connected IPIC6510 instance to reinitialize its state.
+func (s *CartridgeManagerSocket) ResetTrigger() {
+	s.pic.TriggerReset()
+}
+
+// IRQTrigger triggers an interrupt request (IRQ) using the programmable interrupt controller (PIC).
+func (s *CartridgeManagerSocket) IRQTrigger() {
+	s.pic.TriggerIRQ(intrIrqExpansionBit)
+}
+
+// IRQClear clears the IRQ signal associated with the expansion bit from the programmable interrupt controller (PIC).
+func (s *CartridgeManagerSocket) IRQClear() {
+	s.pic.ClearIRQ(intrIrqExpansionBit)
+}
+
+// IRQTriggerBind connects a callback function to the IRQ trigger event, enabling custom handling of IRQ signals.
+func (s *CartridgeManagerSocket) IRQTriggerBind(fn func(uint32)) {
+	s.pic.IRQTriggerBind(fn)
+}
+
+// IRQClearBind binds a callback function that is triggered when the IRQ clear event occurs.
+func (s *CartridgeManagerSocket) IRQClearBind(fn func(uint32)) {
+	s.pic.IRQClearBind(fn)
+}
+
+// BusAvailable checks whether the expansion bus is available by verifying the BA (Bus Available) line status of the VIC.
+func (s *CartridgeManagerSocket) BusAvailable() bool {
+	return !s.vic.GetBALow()
+}
+
+// AECAvailable checks if the AEC (Address Enable Control) line is available and returns true if it's not low.
+func (s *CartridgeManagerSocket) AECAvailable() bool {
+	return !s.vic.GetAECLow()
+}
+
+// Cycle retrieves the current clock cycle count from the associated quartz instance.
+func (s *CartridgeManagerSocket) Cycle() uint64 {
+	return s.quartz.Cycle()
+}
+
+// CycleAlarm creates a new quartz alarm with the given id and callback, and returns the associated IQuartzAlarm instance.
+func (s *CartridgeManagerSocket) CycleAlarm(id string, callback references.QuartzAlarmCallback) references.IQuartzAlarm {
+	return s.quartz.NewAlarm(id, callback)
+}
+
+// RamSetWriteTrigger sets a write trigger callback for a specified memory address, returning an identifier for the trigger.
+func (s *CartridgeManagerSocket) RamSetWriteTrigger(addr uint16, fn func(uint16, uint8)) int {
+	return s.pla.SetWriteTrigger(addr, fn)
+}
+
+// RamRemoveWriteTrigger removes a write trigger callback associated with the specified address and identifier.
+func (s *CartridgeManagerSocket) RamRemoveWriteTrigger(addr uint16, id int) {
+	s.pla.RemoveRamTrigger(addr, id)
+}
+
+// RmwFlags retrieves the Read-Modify-Write flags for CPU operations. Currently, this method is a placeholder for implementation.
+func (s *CartridgeManagerSocket) RmwFlags() uint8 {
+	//TODO IMPLEMENT cpu rmw flags
+	return 0
 }
