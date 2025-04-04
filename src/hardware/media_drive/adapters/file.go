cff8fc87d45220f6d2b2857d8dab78d4dd967f0a
markel1974 <markel@tin.it>
2025-04-04 13:07:33 +0200

    v 2.0

--- src/hardware/media_drive/adapters/file.go
+++ src/hardware/media_drive/adapters/file.go
@@ -4,38 +4,35 @@ import (
 	"os"
 )
 
-// File represents a file system adapter for managing files and directories within a specified path.
-// The `path` field denotes the root directory where operations are performed.
+// File represents a file with an associated path.
 type File struct {
 	path string
 }
 
-// NewFile creates a new AdapterFileSystem instance for the specified directory path.
-// It ensures the provided path ends with a path separator and verifies if the path is a valid directory.
+// NewFile initializes and returns a new File instance for the given path or an error if the operation fails.
 func NewFile(path string) (*File, error) {
 	return &File{path: path}, nil
 }
 
+// Extension returns the file extension of the associated file path.
 func (a *File) Extension() string { return "*" }
 
-// Name returns the path associated with the AdapterFileSystem instance.
+// Name returns the path of the file as a string.
 func (a *File) Name() string {
 	return a.path
 }
 
-// ReadDir reads the content of the directory specified in the AdapterFileSystem and returns a slice of os.FileInfo.
+// ReadDir retrieves the file information of the file at the specified path and returns it in a slice.
 func (a *File) ReadDir() ([]os.FileInfo, error) {
 	s, err := os.Stat(a.path)
 	if err != nil {
 		return nil, err
 	}
-	var out []os.FileInfo
-	out = append(out, s)
+	out := []os.FileInfo{s}
 	return out, nil
 }
 
-// ReadFile reads the contents of a file located at the given path relative to the AdapterFileSystem's base path.
-// It returns the file's data as a byte slice or an error if the file cannot be read.
+// ReadFile reads the content of the file specified by its path and returns the data as a byte slice or an error.
 func (a *File) ReadFile(_ string) ([]byte, error) {
 	data, err := os.ReadFile(a.path)
 	if err != nil {
@@ -44,9 +41,7 @@ func (a *File) ReadFile(_ string) ([]byte, error) {
 	return data, nil
 }
 
-// WriteFile writes the provided data to a file with the specified name within the adapter's file path.
-// It creates the file if it does not exist or overwrites it if it does.
-// Returns an error if the write operation fails.
+// WriteFile writes the provided data to the file at the specified path. Returns an error if the write operation fails.
 func (a *File) WriteFile(_ string, data []byte) error {
 	if err := os.WriteFile(a.path, data, 0644); err != nil {
 		return err
