c8df1c4d795baf2441cb25ff79810efec8c171d4
markel1974 <markel@tin.it>
2025-05-23 16:28:34 +0200

    v 2.0

--- src/renderers/graphics/wasm_render/server/main.go
+++ src/renderers/graphics/wasm_render/server/main.go
@@ -15,36 +15,35 @@ func main() {
 		if err != nil {
 			return err
 		}
-		log.Printf("Trovato in embeddedFiles: %s, Directory: %t\n", path, d.IsDir())
+		log.Printf("found in embeddedFiles: %s, Directory: %t\n", path, d.IsDir())
 		return nil
 	})
 	if err != nil {
-		log.Fatalf("Errore nel listare i file embeddati: %v", err)
+		log.Fatalf("error while listing embedded files: %v", err)
 	}
 
 	subFS, err := fs.Sub(embeddedFiles, "static_content")
 	if err != nil {
-		log.Fatalf("Errore nel creare il sub-filesystem da 'static_content': %v", err)
+		log.Fatalf("error while creating 'static_content': %v", err)
 	}
 
-	log.Println("\n--- Contenuto accessibile tramite 'subFS' (radice del server web) ---")
 	err = fs.WalkDir(subFS, ".", func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
-		// Il path "." qui è la radice di subFS, cioè "static_content"
-		log.Printf("Trovato in subFS (server root): %s, Directory: %t\n", path, d.IsDir())
+		log.Printf("found (server root): %s, directory: %t\n", path, d.IsDir())
 		return nil
 	})
 	if err != nil {
-		log.Fatalf("Errore nel listare i file in subFS: %v", err)
+		log.Fatalf("error whili listing files: %v", err)
 	}
 
 	fileServer := http.FileServer(http.FS(subFS))
 	http.Handle("/", fileServer)
 
-	log.Println("\nServer avviato su http://0.0.0.0:8080")
-	if err = http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal("Errore ListenAndServe: ", err)
+	addr := ":8080"
+	log.Printf("\nstarting server on %s\n", addr)
+	if err = http.ListenAndServe(addr, nil); err != nil {
+		log.Fatal("ListenAndServe error: ", err)
 	}
 }
