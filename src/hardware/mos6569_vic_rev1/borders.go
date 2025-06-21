d743ed5fc35ea6e8c9612aa71c90e45ed040a856
markel1974 <markel@tin.it>
2025-06-21 13:12:57 +0200

    v 2.0

--- src/hardware/mos6569_vic_rev1/borders.go
+++ src/hardware/mos6569_vic_rev1/borders.go
@@ -4,6 +4,21 @@ import (
 	"github.com/markel1974/c64emu/src/references"
 )
 
+const (
+	borderSize       = 8
+	border0Start     = 0
+	border1End       = 4
+	border1Offset    = border1End * borderSize
+	border2Start     = border1End + 1
+	border2End       = 43
+	border3Offset    = border2End * borderSize
+	border4Start     = border2End + 1
+	border4End       = DisplayXDiv8
+	border0StartSize = border0Start * borderSize
+	border2StartSize = border2Start * borderSize
+	border4StartSize = border4Start * borderSize
+)
+
 // BorderType represents the type of border used, defined as an integer-based enumeration.
 type BorderType int
 
@@ -24,25 +39,25 @@ const (
 
 // Borders represents the structure for handling visual border rendering in a VIC-based display system.
 type Borders struct {
-	core             *VIC
-	setMulti8        func(int, uint8)
-	mainFlipFlop     bool
-	verticalFlipFlop bool
-	samples          []bool
-	colors           []uint8
-	offset           int
+	core               *VIC
+	setMulti8          func(int, uint8)
+	horizontalFlipFlop bool
+	verticalFlipFlop   bool
+	samples            []bool
+	colors             []uint8
+	offset             int
 }
 
 // NewBorder creates and initializes a new Borders instance with the provided VIC core and display buffer dependencies.
 func NewBorder(core *VIC, displayBuffer references.IDisplayBuffer) *Borders {
 	gr := &Borders{
-		setMulti8:        displayBuffer.SetMulti8,
-		core:             core,
-		samples:          make([]bool, BorderTypeLast),
-		colors:           make([]uint8, 0xff),
-		mainFlipFlop:     false,
-		verticalFlipFlop: false,
-		offset:           0,
+		setMulti8:          displayBuffer.SetMulti8,
+		core:               core,
+		samples:            make([]bool, BorderTypeLast),
+		colors:             make([]uint8, 0xff),
+		horizontalFlipFlop: false,
+		verticalFlipFlop:   false,
+		offset:             0,
 	}
 	return gr
 }
@@ -54,7 +69,7 @@ func (b *Borders) SetOffset(offset int) {
 
 // AcquireColor updates the color at the specified index in the border's color array using the core's current color configuration.
 func (b *Borders) AcquireColor(idx uint8) {
-	//if b.mainFlipFlop {
+	//if b.horizontalFlipFlop {
 	b.colors[idx] = _colors[b.core.ec]
 	//}
 }
@@ -71,20 +86,20 @@ func (b *Borders) UpdateVerticalFlipFlop() {
 	}
 }
 
-// EnableColumn40 sets the 40-column mode by updating the `mainFlipFlop` if `columnSel` is active and adjusts the right border sample.
+// EnableColumn40 sets the 40-column mode by updating the `horizontalFlipFlop` if `columnSel` is active and adjusts the right border sample.
 func (b *Borders) EnableColumn40() {
 	if b.core.columnSel {
-		b.mainFlipFlop = true
+		b.horizontalFlipFlop = true
 	}
-	b.samples[BorderTypeRight] = b.mainFlipFlop
+	b.samples[BorderTypeRight] = b.horizontalFlipFlop
 }
 
-// EnableColumn38 sets the mainFlipFlop to true if columnSel is false and updates the BorderTypeMidRight sample accordingly.
+// EnableColumn38 sets the horizontalFlipFlop to true if columnSel is false and updates the BorderTypeMidRight sample accordingly.
 func (b *Borders) EnableColumn38() {
 	if !b.core.columnSel {
-		b.mainFlipFlop = true
+		b.horizontalFlipFlop = true
 	}
-	b.samples[BorderTypeMidRight] = b.mainFlipFlop
+	b.samples[BorderTypeMidRight] = b.horizontalFlipFlop
 }
 
 // UpdateColumn40 adjusts the state of the mid-left border based on the column selector and vertical flip-flop status.
@@ -92,10 +107,10 @@ func (b *Borders) UpdateColumn40() {
 	if b.core.columnSel {
 		b.UpdateVerticalFlipFlop()
 		if !b.verticalFlipFlop {
-			b.mainFlipFlop = false
+			b.horizontalFlipFlop = false
 		}
 	}
-	b.samples[BorderTypeMidLeft] = b.mainFlipFlop
+	b.samples[BorderTypeMidLeft] = b.horizontalFlipFlop
 }
 
 // UpdateColumn38 updates the BorderTypeCenter state based on column selection and vertical flip-flop conditions.
@@ -103,10 +118,10 @@ func (b *Borders) UpdateColumn38() {
 	if !b.core.columnSel {
 		b.UpdateVerticalFlipFlop()
 		if !b.verticalFlipFlop {
-			b.mainFlipFlop = false
+			b.horizontalFlipFlop = false
 		}
 	}
-	b.samples[BorderTypeCenter] = b.mainFlipFlop
+	b.samples[BorderTypeCenter] = b.horizontalFlipFlop
 }
 
 // GetVerticalFlipFlop returns the current state of the vertical border flip-flop, indicating its activation status.
@@ -116,27 +131,13 @@ func (b *Borders) GetVerticalFlipFlop() bool {
 
 // Reset reinitializes the left border sample to its main flip-flop state.
 func (b *Borders) Reset() {
-	b.samples[BorderTypeLeft] = b.mainFlipFlop
+	b.samples[BorderTypeLeft] = b.horizontalFlipFlop
 }
 
 // Draw renders the border regions based on current configuration, samples, and colors within the specified display buffer.
 func (b *Borders) Draw() {
-	const bSize = 8
-	const border0Start = 0
-	const border1End = 4
-	const border1Offset = border1End * bSize
-	const border2Start = border1End + 1
-	const border2End = 43
-	const border3Offset = border2End * bSize
-	const border4Start = border2End + 1
-	const border4End = DisplayXDiv8
-
-	const border0StartSize = border0Start * bSize
-	const border2StartSize = border2Start * bSize
-	const border4StartSize = border4Start * bSize
-
 	if b.samples[BorderTypeLeft] {
-		for idx, offset := border0Start, border0StartSize; idx < border1End; idx, offset = idx+1, offset+bSize {
+		for idx, offset := border0Start, border0StartSize; idx < border1End; idx, offset = idx+1, offset+borderSize {
 			b.setMulti8(b.offset+offset, b.colors[idx])
 		}
 	}
@@ -144,7 +145,7 @@ func (b *Borders) Draw() {
 		b.setMulti8(b.offset+(border1Offset), b.colors[border1End])
 	}
 	if b.samples[BorderTypeCenter] {
-		for idx, offset := border2Start, border2StartSize; idx < border2End; idx, offset = idx+1, offset+bSize {
+		for idx, offset := border2Start, border2StartSize; idx < border2End; idx, offset = idx+1, offset+borderSize {
 			b.setMulti8(b.offset+offset, b.colors[idx])
 		}
 	}
@@ -152,7 +153,7 @@ func (b *Borders) Draw() {
 		b.setMulti8(b.offset+(border3Offset), b.colors[border2End])
 	}
 	if b.samples[BorderTypeRight] {
-		for idx, offset := border4Start, border4StartSize; idx < border4End; idx, offset = idx+1, offset+bSize {
+		for idx, offset := border4Start, border4StartSize; idx < border4End; idx, offset = idx+1, offset+borderSize {
 			b.setMulti8(b.offset+offset, b.colors[idx])
 		}
 	}
