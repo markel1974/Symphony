ed8dbd965cd4fd960c4c1fd5f1fef9fcdc4b729f
markel1974 <markel@tin.it>
2025-07-26 08:40:37 +0200

    v 2.0

---
+++ src/benchmark/ticker.go
@@ -0,0 +1,71 @@
+package benchmark
+
+/*
+#include <unistd.h>
+//#include "_cgo_export.h"
+void cticker() {
+    //   usleep(20000); // 20 ms
+	//   Gotask();
+}
+*/
+//
+import (
+	"sync"
+	"time"
+)
+
+type Ticker struct {
+	queue    chan int
+	lock     sync.Mutex
+	emulate  func()
+	interval time.Duration
+	blocks   int
+	cycles   int
+}
+
+func NewTicker(hz int, intervalMs int, blocks int, emulate func()) *Ticker {
+	t := &Ticker{
+		emulate:  emulate,
+		interval: time.Duration(intervalMs) * time.Millisecond,
+		blocks:   blocks,
+		queue:    make(chan int, 4096),
+		cycles:   (hz / intervalMs) / blocks, //runtime.NumCPU(),
+	}
+	return t
+}
+
+func (t *Ticker) loop() {
+	for {
+		select {
+		case cycles := <-t.queue:
+			for step := 0; step < cycles; step++ {
+				t.emulate()
+			}
+		}
+	}
+}
+
+func (t *Ticker) Start() {
+	for {
+		for block := 0; block < t.blocks; block++ {
+			for cycle := 0; cycle < t.cycles; cycle++ {
+				t.emulate()
+			}
+			//runtime.Gosched()
+		}
+		//C.cticker()
+		time.Sleep(time.Duration(20) * time.Millisecond)
+	}
+	/*
+		go t.loop()
+
+		ticker := time.NewTicker(t.interval)
+		defer ticker.Stop()
+		for range ticker.C {
+			for x := 0; x < t.blocks; x++ {
+				t.queue <- t.cycles
+			}
+		}
+
+	*/
+}
---
+++ src/benchmark/vic.go
@@ -0,0 +1,79 @@
+package benchmark
+
+import (
+	"github.com/markel1974/c64emu/src/config"
+	"github.com/markel1974/c64emu/src/hardware"
+	"github.com/markel1974/c64emu/src/hardware/mos6569_vic_rev1"
+	"github.com/markel1974/c64emu/src/renderers/audio/null_audio_render"
+	"github.com/markel1974/c64emu/src/renderers/graphics/null_graphic_render"
+	"log"
+	"os"
+)
+
+type VicSocket struct {
+}
+
+func (v *VicSocket) Cycle() uint64 {
+	return 0
+}
+func (v *VicSocket) BALow(d bool) {
+}
+func (v *VicSocket) AECLow(d bool) {
+}
+func (v *VicSocket) VBlank() {
+}
+func (v *VicSocket) LastCycle() {
+}
+func (v *VicSocket) ScreenFreq() int {
+	return 50
+}
+func (v *VicSocket) TotalRaster() int {
+	return 312
+}
+func (v *VicSocket) ReadRam(addr uint16) uint8 {
+	return 0xff
+}
+func (v *VicSocket) ReadColorRam(addr uint16) uint8 {
+	return 0xff
+}
+func (v *VicSocket) ReadCharRom(addr uint16) uint8 {
+	return 0xff
+}
+func (v *VicSocket) IRQTrigger() {
+}
+func (v *VicSocket) IRQClearTrigger() {
+}
+func (v *VicSocket) RDYLowTrigger(bool) {
+}
+func (v *VicSocket) AECLowTrigger(bool) {
+}
+func (v *VicSocket) LastCycleTrigger() {
+}
+func (v *VicSocket) VBlankTrigger() {
+}
+
+func VIC(hz int, interval int, batch int, components int) {
+	cfg := config.New()
+	audioRender := null_audio_render.NewAudio()
+	displayRender := null_graphic_render.NewDisplayBuffer()
+	hwFactory := hardware.NewFactory(displayRender, audioRender, cfg)
+	var emulate []func()
+	for x := 0; x < components; x++ {
+		vic := mos6569.NewVIC(nil, hwFactory, "test", 0)
+		if err := vic.Setup(); err != nil {
+			log.Fatal(err)
+		}
+		socket := &VicSocket{}
+		if err := vic.Bind(socket); err != nil {
+			log.Fatal(err)
+		}
+		emulate = append(emulate, vic.Emulate)
+	}
+	ticker := NewTicker(hz, interval, batch, func() {
+		for _, v := range emulate {
+			v()
+		}
+	})
+	ticker.Start()
+	os.Exit(1)
+}
