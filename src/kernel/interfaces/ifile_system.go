71038f17d9d76170b5e85963fd0888685ad5b698
markel1974 <markel@tin.it>
2025-08-07 16:43:43 +0200

    v 2.0

--- src/kernel/interfaces/ifile_system.go
+++ src/kernel/interfaces/ifile_system.go
@@ -3,10 +3,6 @@ package interfaces
 type IFileSystem interface {
 	IServer
 
-	//CallCWDSet(router IRouter, arg string) bool
-
-	//CallCWDName(router IRouter) string
-
 	CallCWDPath(router IRouter) string
 
 	CallCWDDirectoryListing(router IRouter) []string
@@ -14,6 +10,4 @@ type IFileSystem interface {
 	CallFind(router IRouter, line string) (ICommand, []string, error)
 
 	CallHelp(router IRouter, path string) (string, error)
-
-	//CallSuggestion(router IRouter, in string, cursor int) (string, []string, bool)
 }
