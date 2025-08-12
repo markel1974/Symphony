049d887fbfde146d9b997136d723f2aed8d2d8ff
markel1974 <markel@tin.it>
2025-08-12 13:48:40 +0200

    v 2.0

--- src/kernel/compiler/source_file.go
+++ src/kernel/compiler/source_file.go
@@ -1,26 +1,31 @@
 package compiler
 
-// SourceFile represents a source file in a SourceFileSet.
-// It contains metadata such as name, size, base offset, and line offsets.
+import (
+	"fmt"
+
+	"github.com/markel1974/c64emu/src/kernel/vm/bytecode"
+)
+
+// SourceFile represents a source file in a collection, storing its positional metadata and line offsets.
 type SourceFile struct {
-	set   *SourceFileSet
+	set   *bytecode.Files
 	name  string
 	base  int
 	size  int
 	lines []int
 }
 
-// Set returns the SourceFileSet associated with the SourceFile.
-func (f *SourceFile) Set() *SourceFileSet {
+// Set returns the Files associated with the SourceFile.
+func (f *SourceFile) Set() *bytecode.Files {
 	return f.set
 }
 
-// LineCount returns the total number of lines in the source file.
+// LineCount returns the total number of lines in the SourceFile.
 func (f *SourceFile) LineCount() int {
 	return len(f.lines)
 }
 
-// AddLine adds a new line offset to the source file if it is valid and greater than the previous line offset.
+// AddLine appends a new line offset to the SourceFile if valid, ensuring offsets are in ascending order and within size.
 func (f *SourceFile) AddLine(offset int) {
 	i := len(f.lines)
 	if (i == 0 || f.lines[i-1] < offset) && offset < f.size {
@@ -28,58 +33,61 @@ func (f *SourceFile) AddLine(offset int) {
 	}
 }
 
-// LineStart returns the Pos value of the first character in the specified line number of the SourceFile.
-// The provided line number must be greater than 0 and within the range of valid lines in the file.
-func (f *SourceFile) LineStart(line int) Pos {
+// LineStart returns the start position (offset) of the specified line within the source file. Panics for invalid line numbers.
+func (f *SourceFile) LineStart(line int) int {
 	if line < 1 {
 		panic("illegal line number (line numbering starts at 1)")
 	}
 	if line > len(f.lines) {
 		panic("illegal line number")
 	}
-	return Pos(f.base + f.lines[line-1])
+	return f.base + f.lines[line-1]
 }
 
-// FileSetPos returns the positional value for a given offset in the source file.
-// It panics if the provided offset exceeds the file size.
-func (f *SourceFile) FileSetPos(offset int) Pos {
+// FileSetPos calculates and returns the absolute position in the file set for the given file-relative offset.
+// Panics if the offset exceeds the file size.
+func (f *SourceFile) FileSetPos(offset int) int {
 	if offset > f.size {
 		panic("illegal file offset")
 	}
-	return Pos(f.base + offset)
+	return f.base + offset
 }
 
-// Offset returns the absolute offset for a given position within the SourceFile. Panics if the position is invalid.
-func (f *SourceFile) Offset(p Pos) int {
+// Offset computes the offset of a position p relative to the start of the SourceFile, panic on illegal values.
+func (f *SourceFile) Offset(p int) int {
 	if int(p) < f.base || int(p) > f.base+f.size {
 		panic("illegal SourcePos values")
 	}
 	return int(p) - f.base
 }
 
-// Position translates a Pos value into a SourceFilePos, containing detailed position information within the file.
-// It panics if the given position is invalid or out of range for the source file.
-func (f *SourceFile) Position(p Pos) (pos SourceFilePos) {
-	if p != NoPos {
-		if int(p) < f.base || int(p) > f.base+f.size {
-			panic("illegal SourcePos values")
-		}
-		pos = f.position(p)
+// Position returns the position information (filename, offset, line, and column) for a given identifier `p` in the SourceFile.
+// It returns an error if `p` is invalid, out of range, or set to `NoPos`.
+// The resulting position is represented using a `FilePos` object populated with the corresponding details.
+func (f *SourceFile) Position(p int) (*bytecode.FilePos, error) {
+	if p == 0 {
+		return nil, fmt.Errorf("illegal NoPos value")
 	}
-	return
+	if p < f.base || p > f.base+f.size {
+		return nil, fmt.Errorf("illegal SourcePos values")
+	}
+	offset := p - f.base
+	filename, line, column := f.unpack(offset)
+	pos := bytecode.NewSourceFilePos(filename, offset, line, column)
+	return pos, nil
 }
 
-// position computes the SourceFilePos from a given Pos by unpacking its offset relative to the SourceFile base.
-func (f *SourceFile) position(p Pos) (pos SourceFilePos) {
-	offset := int(p) - f.base
-	pos.offset = offset
-	pos.filename, pos.line, pos.column = f.unpack(offset)
-	return
+// Base returns the base offset of the SourceFile in its Files.
+func (f *SourceFile) Base() int {
+	return f.base
 }
 
-// unpack determines the filename, line, and column for a given offset in the source file.
-// It returns the file name, line number (starting at 1), and column number (starting at 1).
-// The method uses searchInts to locate the corresponding line in the offset data.
+// Size returns the size of the source file in bytes.
+func (f *SourceFile) Size() int {
+	return f.size
+}
+
+// unpack calculates the filename, line, and column for a given offset in the source file.
 func (f *SourceFile) unpack(offset int) (filename string, line, column int) {
 	filename = f.name
 	if i := searchInts(f.lines, offset); i >= 0 {
@@ -88,7 +96,8 @@ func (f *SourceFile) unpack(offset int) (filename string, line, column int) {
 	return
 }
 
-// searchInts searches for the largest index `i` in a sorted slice `a` such that `a[i] <= x` and returns that index.
+// searchInts performs a binary search on a sorted slice of integers `a` to find the largest index `i` where a[i] <= x.
+// Returns -1 if no such index exists. The input slice `a` must be sorted in ascending order.
 func searchInts(a []int, x int) int {
 	// This function body is a manually inlined version of:
 	//   return sort.Search(len(a), func(i int) bool { return a[i] > x }) - 1
