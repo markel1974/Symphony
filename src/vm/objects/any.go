86c72e35b89f8a6a208ad459da90df80d24c8394
markel1974 <markel@tin.it>
2025-09-17 08:31:29 +0200

    release

--- src/vm/objects/any.go
+++ src/vm/objects/any.go
@@ -5,7 +5,7 @@ import (
 	"reflect"
 )
 
-// Any is a flexible type wrapping a Go-native value, offering reflection-based type introspection and manipulation.
+// Any represents a flexible polymorphic type that wraps and operates on arbitrary data using reflection and allocation.
 type Any struct {
 	IAllocator
 	data interface{}
@@ -13,7 +13,7 @@ type Any struct {
 	t    reflect.Type
 }
 
-// newAny creates a new instance of Any, initializing it with the provided allocator and value.
+// newAny creates a new instance of Any, encapsulating the provided value and associating it with the specified allocator.
 func newAny(allocator IAllocator, value interface{}) IObject {
 	valOf := reflect.ValueOf(value)
 	return &Any{
@@ -24,17 +24,17 @@ func newAny(allocator IAllocator, value interface{}) IObject {
 	}
 }
 
-// setAllocator assigns a custom memory allocator for managing lifecycle and memory references of the object.
+// setAllocator assigns the specified IAllocator to the Any instance for memory management and object lifecycle control.
 func (o *Any) setAllocator(allocator IAllocator) {
 	o.IAllocator = allocator
 }
 
-// TypeName returns the string representation of the cached reflected type of the object.
+// TypeName returns the name of the underlying type as a string.
 func (o *Any) TypeName() string {
 	return o.t.String()
 }
 
-// AsString converts the stored data in the Any object to a string representation. If the data is nil, it returns "<nil>".
+// AsString returns the string representation of the wrapped data. If the data is nil, it returns "<nil>".
 func (o *Any) AsString() string {
 	if o.data == nil {
 		return "<nil>"
@@ -42,12 +42,12 @@ func (o *Any) AsString() string {
 	return fmt.Sprintf("%v", o.data)
 }
 
-// AsBool returns the boolean representation of the underlying value, interpreted as !o.Falsy().
+// AsBool returns the boolean representation of the Any object by negating its Falsy status.
 func (o *Any) AsBool() bool {
 	return !o.Falsy()
 }
 
-// AsInt64 converts the value held by the Any object to an int64 if possible; otherwise, it returns 0.
+// AsInt64 converts the underlying value to an int64 if possible, otherwise returns 0.
 func (o *Any) AsInt64() int64 {
 	if o.v.CanInt() {
 		return o.v.Int()
@@ -55,7 +55,7 @@ func (o *Any) AsInt64() int64 {
 	return 0
 }
 
-// AsFloat64 returns the value of the Any object as a float64 if it is convertible; otherwise, it returns 0.
+// AsFloat64 returns the value of `Any` as a float64 if convertible, otherwise returns 0.
 func (o *Any) AsFloat64() float64 {
 	if o.v.CanFloat() {
 		return o.v.Float()
@@ -63,7 +63,7 @@ func (o *Any) AsFloat64() float64 {
 	return 0
 }
 
-// Falsy checks if the contained value is considered "falsy." Returns true if the value is nil or has a zero value.
+// Falsy checks if the underlying data is nil or if the value is considered zero, returning true in those cases.
 func (o *Any) Falsy() bool {
 	if o.data == nil {
 		return true
@@ -71,7 +71,7 @@ func (o *Any) Falsy() bool {
 	return o.v.IsZero()
 }
 
-// Equals compares the current instance with another IObject and returns true if they are equivalent, false otherwise.
+// Equals compares the current object with another IObject and returns true if they are considered equal, otherwise false.
 func (o *Any) Equals(other IObject) bool {
 	if otherAny, ok := other.(*Any); ok {
 		return o.data == otherAny.data
@@ -79,73 +79,37 @@ func (o *Any) Equals(other IObject) bool {
 	return false
 }
 
-// Copy creates a new instance of the object with the provided frame and retains the original data.
+// Copy creates a new instance of Any with the same data as the current object and associates it with the given frame.
 func (o *Any) Copy(frame int, _ int) IObject {
 	return o.GateKeeper().NewAny(frame, o.data)
 }
 
-// IndexGet retrieves the value of a field or method in the object based on the provided index as key.
+// IndexGet retrieves a field or method of a struct or pointer to a struct by its name, provided as the index parameter.
+// Returns an error if the index is not a valid key or if the field/method cannot be found.
+// If the index refers to a method, the returned IObject will be callable.
 func (o *Any) IndexGet(frame int, index IObject) (IObject, error) {
 	key, ok := o.GateKeeper().ToString(index)
 	if !ok {
 		return nil, ErrIndexInvalidType
 	}
-
 	v := o.v
 	if v.Kind() == reflect.Ptr {
-		v = v.Elem() // Dereference pointer to access fields
+		v = v.Elem()
 	}
-	// 1. Try to access a struct field
 	if v.Kind() == reflect.Struct {
 		field := v.FieldByName(key)
 		if field.IsValid() {
 			return o.GateKeeper().FromInterface(frame, field.Interface()), nil
 		}
 	}
-	// 2. If not a field, try to find a method
-	method := o.v.MethodByName(key)
-	if method.IsValid() {
-		// Wrap the Go method in a function that the VM can call
-		return o.GateKeeper().NewFuncImport(frame, key, -1, func(gk IGateKeeper, f int, args ...IObject) (uint, IObject, error) {
-			// Convert IObject arguments to reflect.Value
-			if method.Type().NumIn() != len(args) {
-				return 0, nil, fmt.Errorf("wrong number of arguments for method %s: want %d, got %d", key, method.Type().NumIn(), len(args))
-			}
-			in := make([]reflect.Value, len(args))
-			for i, arg := range args {
-				in[i] = reflect.ValueOf(gk.ToInterface(arg))
-			}
-			// Call the native Go method
-			results := method.Call(in)
-
-			// Convert results back to IObject
-			if len(results) == 0 {
-				return 0, gk.UndefinedValue(), nil
-			}
-			// Simplification: handle single return value, but could be extended
-			if len(results) == 1 {
-				// Special handling for 'error' type
-				if err, isErr := results[0].Interface().(error); isErr {
-					if err != nil {
-						return 1, gk.NewError(f, err.Error()), nil
-					}
-					return 1, gk.UndefinedValue(), nil // nil error
-				}
-				return 1, gk.FromInterface(f, results[0].Interface()), nil
-			}
-			// Handle multiple return values by packing them into an Array
-			retArray := make([]IObject, len(results))
-			for i, res := range results {
-				retArray[i] = gk.FromInterface(f, res.Interface())
-			}
-			return 1, gk.NewArray(f, retArray), nil
-		}), nil
+	fn, ok := o.buildFunc(frame, key)
+	if ok {
+		return fn, nil
 	}
 	return nil, fmt.Errorf("field or method '%s' not found on type '%s'", key, o.TypeName())
 }
 
-// IndexSet sets a value for a specific index on the object, usually by modifying a field of a struct via reflection.
-// It returns an error if the object is not a pointer to a struct, the index type is not valid, or the field cannot be set.
+// IndexSet updates the value of a struct field, specified by the index, with the given value if the field is assignable.
 func (o *Any) IndexSet(index IObject, value IObject) error {
 	if o.v.Kind() != reflect.Ptr || o.v.Elem().Kind() != reflect.Struct {
 		return ErrNotAssignable // Puoi modificare solo i campi di un puntatore a struct
@@ -163,53 +127,69 @@ func (o *Any) IndexSet(index IObject, value IObject) error {
 		field.Set(valToSet)
 		return nil
 	}
-
 	return fmt.Errorf("cannot assign type %s to field %s (type %s)", valToSet.Type(), key, field.Type())
 }
 
-// AssignValue assigns the value of the given IObject to the current object. Returns an error if assignment is not supported.
-func (o *Any) AssignValue(v IObject) error {
+// AssignValue assigns a value from another IObject but always returns ErrNotAssignable indicating the operation is unsupported.
+func (o *Any) AssignValue(_ IObject) error {
 	return ErrNotAssignable
 }
 
-// Nil returns true if the underlying data of the Any object is nil, otherwise false.
+// Nil checks if the `data` field of the `Any` object is nil and returns true if it is, otherwise false.
 func (o *Any) Nil() bool {
 	return o.data == nil
 }
 
-// LogicalOp performs a logical operation on the current object using the specified operator and right-hand operand.
-// It returns the result of the operation or an error if the operation is not supported.
+// LogicalOp performs a logical operation on the current object with the provided operator and right-hand side object.
+// It returns the result of the operation if the operator is valid, otherwise it returns an ErrInvalidOperator error.
 func (o *Any) LogicalOp(_ int, _ LogicalOperator, _ IObject) (IObject, error) {
 	return nil, ErrInvalidOperator
 }
 
-// ArithmeticOp performs an arithmetic operation using the given operator and right-hand-side operand.
-// Returns an error if the operation is not supported or invalid.
+// ArithmeticOp performs an arithmetic operation on the current object and the provided IObject using the specified operator.
+// Returns the result as an IObject or an error if the operation is invalid.
 func (o *Any) ArithmeticOp(_ int, _ ArithmeticOperator, _ IObject) (IObject, error) {
 	return nil, ErrInvalidOperator
 }
 
-// Iterate returns an IIterator instance for iterating over the underlying data. Always returns nil for non-iterable types.
+// Iterate returns an iterator (IIterator) that traverses the object's data. Returns nil if the object is not iterable.
 func (o *Any) Iterate(_ int) IIterator {
 	return nil
 }
 
-// Iterable determines if the object can be iterated over, returning false if iteration is not supported.
+// Iterable checks if the object is iterable and returns false, indicating it is not iterable by default.
 func (o *Any) Iterable() bool {
 	return false
 }
 
-// Call invokes the object as a callable with the specified frame and arguments.
-// Returns the execution status, result object, and an error if the object is not callable.
+// Call invokes a function or a method on the object with the provided arguments and returns the result or an error.
 func (o *Any) Call(frame int, args ...IObject) (uint, IObject, error) {
 	if o.v.Kind() == reflect.Func {
-		v, _ := o.IndexGet(frame, o.GateKeeper().NewString(frame, ""))
-		return 0, v, nil
+		fnType := o.t
+		if fnType.NumIn() != len(args) && !fnType.IsVariadic() {
+			return 0, nil, fmt.Errorf("wrong number of arguments for %s: want %d, got %d", o.TypeName(), fnType.NumIn(), len(args))
+		}
+		return o.call(o.v, o.GateKeeper(), frame, args)
 	}
-	return 0, nil, fmt.Errorf("object of type %s is not callable", o.TypeName())
+	if len(args) == 0 {
+		return 0, nil, nil
+	}
+	key, ok := o.GateKeeper().ToString(args[0])
+	if !ok {
+		return 0, nil, ErrInvalidArgumentsNumber
+	}
+	method := o.v.MethodByName(key)
+	if !method.IsValid() {
+		return 0, nil, fmt.Errorf("function not found on type '%s'", o.TypeName())
+	}
+	arguments := args[1:]
+	if method.Type().NumIn() != len(arguments) && !method.Type().IsVariadic() {
+		return 0, nil, fmt.Errorf("wrong number of arguments for method %s: want %d, got %d", key, method.Type().NumIn(), len(args))
+	}
+	return o.call(o.v, o.GateKeeper(), frame, arguments)
 }
 
-// Length returns the length of the underlying value if it is an Array, Chan, Map, Slice, or String. Otherwise, it returns 0.
+// Length returns the length of the underlying value if it is an array, channel, map, slice, or string; otherwise, 0.
 func (o *Any) Length() int {
 	switch o.v.Kind() {
 	case reflect.Array, reflect.Chan, reflect.Map, reflect.Slice, reflect.String:
@@ -219,7 +199,53 @@ func (o *Any) Length() int {
 	}
 }
 
-// Count returns the static value of 1 for an Any instance.
+// Count returns a fixed integer value of 1, representing a single object.
 func (o *Any) Count() int {
 	return 1
+}
+
+// buildFunc defines and returns a method wrapper for the specified key if it exists, otherwise returns false.
+func (o *Any) buildFunc(frame int, key string) (IObject, bool) {
+	method := o.v.MethodByName(key)
+	if !method.IsValid() {
+		return nil, false
+	}
+	return o.GateKeeper().NewFuncImport(frame, key, -1, func(gk IGateKeeper, f int, args ...IObject) (uint, IObject, error) {
+		if method.Type().NumIn() != len(args) {
+			return 0, nil, fmt.Errorf("wrong number of arguments for method %s: want %d, got %d", key, method.Type().NumIn(), len(args))
+		}
+		return o.call(method, gk, f, args)
+	}), true
+}
+
+// call invokes a reflect.Value method with arguments, handles results, and converts to/from IObject representation.
+func (o *Any) call(method reflect.Value, gk IGateKeeper, frame int, args []IObject) (uint, IObject, error) {
+	// Convert IObject arguments to reflect.Value
+	in := make([]reflect.Value, len(args))
+	for i, arg := range args {
+		in[i] = reflect.ValueOf(gk.ToInterface(arg))
+	}
+	// Call the native Go method
+	results := method.Call(in)
+	// Convert results back to IObject
+	if len(results) == 0 {
+		return 0, gk.UndefinedValue(), nil
+	}
+	// Simplification: handle single return value, but could be extended
+	if len(results) == 1 {
+		// Special handling for 'error'
+		if err, isErr := results[0].Interface().(error); isErr {
+			if err != nil {
+				return 1, gk.NewError(frame, err.Error()), nil
+			}
+			return 1, gk.UndefinedValue(), nil // nil error
+		}
+		return 1, gk.FromInterface(frame, results[0].Interface()), nil
+	}
+	// Handle multiple return values by packing them into an Array
+	retArray := make([]IObject, len(results))
+	for i, res := range results {
+		retArray[i] = gk.FromInterface(frame, res.Interface())
+	}
+	return 1, gk.NewArray(frame, retArray), nil
 }
