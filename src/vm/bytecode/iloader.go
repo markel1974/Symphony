2c517d8ac7686375e02cf94f4e4aa8c59dc3a0f9
markel1974 <markel@tin.it>
2025-09-20 09:53:15 +0200

    release

--- src/vm/bytecode/iloader.go
+++ src/vm/bytecode/iloader.go
@@ -4,6 +4,15 @@ import (
 	"github.com/markel1974/c64emu/src/vm/objects"
 )
 
+// IPackage represents a package interface that provides access to package-level functionality or resources.
+// Name retrieves the name of the package.
+// Get retrieves a specific object from the package by its name, returning the object and a boolean indicating success.
+type IPackage interface {
+	Name() string
+
+	Get(name string) (objects.IObject, bool)
+}
+
 // RegisterPackageFn is a function type defining a method that registers a package using IGateKeeper and returns an IPackage.
 type RegisterPackageFn func(f objects.IGateKeeper) IPackage
 
--- src/vm/bytecode/package.go
+++ src/vm/bytecode/package.go
@@ -2,13 +2,13 @@ package bytecode
 
 import "github.com/markel1974/c64emu/src/vm/objects"
 
-// Package represents a module with a name and a collection of named objects.
+// Package represents a container for storing and managing named objects implementing the IObject interface.
 type Package struct {
 	name      string
 	container map[string]objects.IObject
 }
 
-// NewPackage creates and returns a new Package instance with the specified name and attribute mapping.
+// NewPackage creates and returns a new instance of Package with the specified name and an initialized container map.
 func NewPackage(name string) *Package {
 	pkg := &Package{
 		name:      name,
@@ -17,6 +17,7 @@ func NewPackage(name string) *Package {
 	return pkg
 }
 
+// Add associates the given id with the provided IObject in the package's container.
 func (p *Package) Add(id string, obj objects.IObject) {
 	p.container[id] = obj
 }
@@ -26,7 +27,7 @@ func (p *Package) Name() string {
 	return p.name
 }
 
-// Get retrieves an object from the package's container using the given id and returns the object and a boolean indicating success.
+// Get retrieves an object by its ID from the package's container. It returns the object and a boolean indicating existence.
 func (p *Package) Get(id string) (objects.IObject, bool) {
 	v, ok := p.container[id]
 	return v, ok
