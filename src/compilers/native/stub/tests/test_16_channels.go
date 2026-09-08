70384faa80174b158722cc773becde87b8aef557
markel1974 <markel@tin.it>
2026-09-08 17:01:07 +0200

    release

--- src/compilers/native/stub/tests/test_16_channels.go
+++ src/compilers/native/stub/tests/test_16_channels.go
@@ -6,26 +6,26 @@ import (
 
 func worker(c chan int, val int) {
 	// fmt.Println("Worker processing value:", val)
-	c <- val * 2
+	c <- val
 }
 
 func main() {
 	fmt.Println("--- Running Test: Channels Pattern ---")
-
+	const v1, v2, v3 = 10, 20, 30
 	// 1. Test Buffered Channel
 	bufChan := make(chan int, 2)
-	bufChan <- 10
-	bufChan <- 20
+	bufChan <- v1
+	bufChan <- v2
 	val1 := <-bufChan
 	val2 := <-bufChan
 
 	// 2. Test Unbuffered Channel with Goroutine
 	unbufChan := make(chan int, 0)
-	go worker(unbufChan, 21)
+	go worker(unbufChan, v3)
 	val3 := <-unbufChan
 
 	finalValue := val1 + val2 + val3
-	expectedValue := 10 + 20 + 42
+	expectedValue := v1 + v2 + v3
 
 	if finalValue == expectedValue {
 		fmt.Println("[TEST PASSED] Channels and Goroutines worked correctly.")
